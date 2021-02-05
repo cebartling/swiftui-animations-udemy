43b38ca8521789e247b31d450c67e97575fcc7e4
Christopher Bartling <chris.bartling@gmail.com>
2021-02-05 13:43:52 -0600

    Animating Circles project completed

---
+++ Animating Circles/Animating Circles/Animating_CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animating_CirclesApp.swift
+//  Animating Circles
+//
+//  Created by Christopher Bartling on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Animating_CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
