61e6a5e431f5bb79f0453f61decf3386411b2816
Christopher Bartling <chris.bartling@gmail.com>
2021-02-07 21:58:55 -0600

    SpriteKit Animation completed

---
+++ SpriteKit Animation/SpriteKit Animation/SpriteKit_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKit_AnimationApp.swift
+//  SpriteKit Animation
+//
+//  Created by Christopher Bartling on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKit_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
